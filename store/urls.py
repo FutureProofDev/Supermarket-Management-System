378cbc91d59f085e48b0d992bbcba7a870e2d5ab
Nii Sowah Kwabla Sowah <nii.sowah@ashesi.edu.gh>
2026-08-12 11:56:15 +0000

    Provide URL routing for the store app in the project.

---
+++ store/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('categories/', views.category_list, name='category_list'),
+    path('categories/new/', views.category_create, name='category_create'),
+    path('categories/<int:pk>/', views.category_detail, name='category_detail'),
+    path('categories/<int:pk>/edit/', views.category_update, name='category_update'),
+    path('categories/<int:pk>/delete/', views.category_delete, name='category_delete'),
+
+]
